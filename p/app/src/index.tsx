b5e97e064a764f21f99ecc7b2a136cf82caede67
elado <elad.ossadon@databricks.com>
2021-08-18 11:04:40 -0700

    ts refs working

---
+++ p/app/src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { UI } from "@x/ui";
+
+export function App() {
+  return (
+    <div>
+      App <UI />
+    </div>
+  );
+}
---
+++ p/ui/src/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function UI() {
+  return <div>UI</div>;
+}
